b472b172c654b72b919972c37d11e0637e29fa97
Bruno Dluka Antunes Viana Oliveira <brunodlukaa@dinfor.uniceub>
2023-05-19 09:25:38 -0300

    add missing features

--- Trabalho.go
+++ Trabalho.go
@@ -4,43 +4,63 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
-	"time"
 )
+
+var playAttempts []int
 
 func main() {
 
 	fmt.Println("Bem-vindo ao jogo da adivinhação!")
+	play()
 
-	rand.Seed(time.Now().UnixNano())
-	Resposta := rand.Intn(100)
+}
+
+func play() {
+	num := rand.Intn(100)
+	var attempts int
 	for {
 
-		var x int
-		fmt.Println("Digite um Numero")
-		fmt.Scanln(&x)
-
-		if x == Resposta {
-			fmt.Println("certo")
-		} else if x < Resposta {
-			fmt.Println("o numero e maior")
+		var entry int
+		fmt.Println("Digite um número entre 1 e 100:")
+		_, err := fmt.Scan(&entry)
+		if err != nil {
+			fmt.Println("Digite um número valido.")
+		} else if entry == num {
+			fmt.Println("Parabéns, você acertou!")
+			break
+		} else if entry < num {
+			fmt.Printf("O número é maior que %d\n", entry)
 		} else {
-			fmt.Println("O numero e menor")
-
+			fmt.Printf("O número é menor que %d\n", entry)
 		}
+
+		attempts++
 
 	}
 
-	jogarnovamente()
+	fmt.Printf("Você utilizou %d tentativas\n", attempts)
+	playAttempts = append(playAttempts, attempts)
+
+	playAgain()
 }
 
-func jogarnovamente() {
-	fmt.Print("Deseja jogar novamente? (sim/não): ")
-	var carol string
-	fmt.Scanln(&carol)
-	carol = strings.ToLower(strings.TrimSpace(carol))
-	if carol == "sim" {
-		jogarnovamente()
+func playAgain() {
+	fmt.Println("Deseja jogar novamente? (sim/não): ")
+
+	var answer string
+	_, err := fmt.Scan(&answer)
+	if err != nil {
+		fmt.Println("Desculpe, um erro ocorreu! :/")
+		return
+	}
+	answer = strings.ToLower(strings.TrimSpace(answer))
+
+	if answer == "sim" || answer == "s" || answer == "yes" || answer == "si" || answer == "y" {
+		play()
 	} else {
-		fmt.Println("tks por jogar")
+		fmt.Println("\nObrigado por jogar :)")
+		for i, attempt := range playAttempts {
+			fmt.Printf("Você utilizou %d tentativas na %d jogada\n", attempt, i+1)
+		}
 	}
 }
